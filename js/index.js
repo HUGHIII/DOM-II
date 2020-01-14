bd089ea3c598f58027c637271c961b16a86c250a
TREY <hugh.a.lloyd@gmail.com>
2020-01-14 16:59:21 -0600

    prog

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+const navEvents = document.querySelectorAll('.nav-link');
+
+navEvents.forEach( element => {
+    element.addEventListener('mouseenter',() =>{
+        element.style.color = 'blue';
+        
+    })
+});
+
+navEvents.forEach( element => {
+    element.addEventListener('mouseleave',() =>{
+        
+        element.style.color = 'black';
+    })
+});
+
+
+
+ 
