fda380275aa4fa53117931f83b94eea42c616c63
TREY <hugh.a.lloyd@gmail.com>
2020-01-14 18:06:50 -0600

    3events...

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // Your code goes here
+
+
+//nav events mouseover
 const navEvents = document.querySelectorAll('.nav-link');
 
 navEvents.forEach( element => {
@@ -14,6 +17,25 @@ navEvents.forEach( element => {
         element.style.color = 'black';
     })
 });
+
+
+//img resize
+
+const newHeadImg = document.querySelector('.intro img');
+
+window.addEventListener('resize', () => {
+    newHeadImg.src = 'https://images.pexels.com/photos/385998/pexels-photo-385998.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+});
+
+// img dblclick
+
+const newImgLetsGo = document.querySelector('.img-content img');
+
+newImgLetsGo.addEventListener('dblclick',   () => {
+    newImgLetsGo.src = 'https://images.pexels.com/photos/1647962/pexels-photo-1647962.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+});
+
+
 
 
 
